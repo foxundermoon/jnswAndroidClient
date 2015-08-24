66cf6853741cfb626d623635ab8b2d43a248690d
fox <fox@vvfox.com>
2015-08-24 10:42:06 +0800

    fiex the StickDrawerLayout but can not stick

--- coredemo/src/main/java/com/jnsw/coredemo/StickDrawerLayoutDemo.java
+++ coredemo/src/main/java/com/jnsw/coredemo/StickDrawerLayoutDemo.java
@@ -1,20 +1,39 @@
 package com.jnsw.coredemo;
 
+import android.graphics.Color;
+import android.support.v4.widget.StickDrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
+import org.androidannotations.annotations.NoTitle;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.WindowFeature;
 
+@Fullscreen
+@WindowFeature({Window.FEATURE_NO_TITLE})
 @EActivity(R.layout.activity_stick_drawer_layout_demo)
 public class StickDrawerLayoutDemo extends AppCompatActivity {
 
+    @ViewById(R.id.stick_drawer_layout_demo)
+    StickDrawerLayout stickDrawerLayout;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_stick_drawer_layout_demo, menu);
         return true;
+    }
+
+    @AfterViews
+    void init(){
+        stickDrawerLayout.setScrimColor(Color.TRANSPARENT);
+        stickDrawerLayout.setDrawerLockMode(StickDrawerLayout.LOCK_MODE_UNLOCKED);
+
     }
 
     @Override
